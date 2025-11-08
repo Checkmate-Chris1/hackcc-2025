32ee59125a4e36a64b53b0a6a40790b920523c27
CheckmateChris1 <checkdoesgames@gmail.com>
2025-11-08 15:29:44 -0800

    3d-ified results page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ResultsPage from './pages/ResultsPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <ResultsPage />
+        {/* <App /> */}
     </StrictMode>,
 )
